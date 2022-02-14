2a3f1e032e0ca1c5fdc12746440ec464f426f522
Hao Dinh Nguyen <80563241+haodngn@users.noreply.github.com>
2022-02-14 12:18:37 +0700

    Update EventErrorDTO.java

--- src/java/dto/EventErrorDTO.java
+++ src/java/dto/EventErrorDTO.java
@@ -5,10 +5,93 @@
  */
 package dto;
 
+import java.io.Serializable;
+
 /**
  *
  * @author HAO
  */
-public class EventErrorDTO {
+public class EventErrorDTO implements Serializable{
+    private String isEmpty;
+    private String speakerLength;
+    private String nameLength;
+    private String desLength;
+    private String locationLength;
+    private String registerDateCheck;
+    private String expDateCheck;
+    private String occurDateCheck;
+    private String endDateCheck;
+
+    public String getIsEmpty() {
+        return isEmpty;
+    }
+
+    public void setIsEmpty(String isEmpty) {
+        this.isEmpty = isEmpty;
+    }
+
+    public String getSpeakerLength() {
+        return speakerLength;
+    }
+
+    public void setSpeakerLength(String speakerLength) {
+        this.speakerLength = speakerLength;
+    }
+
+    public String getNameLength() {
+        return nameLength;
+    }
+
+    public void setNameLength(String nameLength) {
+        this.nameLength = nameLength;
+    }
+
+    public String getDesLength() {
+        return desLength;
+    }
+
+    public void setDesLength(String desLength) {
+        this.desLength = desLength;
+    }
+
+    public String getLocationLength() {
+        return locationLength;
+    }
+
+    public void setLocationLength(String locationLength) {
+        this.locationLength = locationLength;
+    }
+
+    public String getRegisterDateCheck() {
+        return registerDateCheck;
+    }
+
+    public void setRegisterDateCheck(String registerDateCheck) {
+        this.registerDateCheck = registerDateCheck;
+    }
+
+    public String getExpDateCheck() {
+        return expDateCheck;
+    }
+
+    public void setExpDateCheck(String expDateCheck) {
+        this.expDateCheck = expDateCheck;
+    }
+
+    public String getOccurDateCheck() {
+        return occurDateCheck;
+    }
+
+    public void setOccurDateCheck(String occurDateCheck) {
+        this.occurDateCheck = occurDateCheck;
+    }
+
+    public String getEndDateCheck() {
+        return endDateCheck;
+    }
+
+    public void setEndDateCheck(String endDateCheck) {
+        this.endDateCheck = endDateCheck;
+    }
     
 }
